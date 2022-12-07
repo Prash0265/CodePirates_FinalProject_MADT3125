7921ea080466115d93833a51a67d2e2358a773e1
Aditya <adiranjan333>
2022-12-06 21:40:46 -0500

    Created first screen

---
+++ app/src/main/java/com/example/codepirates_finalproject_madt3125/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.codepirates_finalproject_madt3125;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
