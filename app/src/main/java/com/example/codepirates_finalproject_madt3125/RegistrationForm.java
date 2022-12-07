87fc45ebcf20e8f2bca69b770f067f5b07f10d82
AdityaRanjan466 <C0880466@mylambton.ca>
2022-12-07 18:25:54 -0500

    Created registration form screen and connected to button

--- app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
+++ app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
@@ -1,6 +1,7 @@
 package com.example.codepirates_finalproject_madt3125;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class RegistrationForm extends AppCompatActivity {
