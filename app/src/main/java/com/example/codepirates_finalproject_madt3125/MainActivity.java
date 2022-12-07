5e21ad89718a933fa95224cc785b97e9f99b109f
AdityaRanjan466 <C0880466@mylambton.ca>
2022-12-06 21:53:41 -0500

    Created registration form screen

--- app/src/main/java/com/example/codepirates_finalproject_madt3125/MainActivity.java
+++ app/src/main/java/com/example/codepirates_finalproject_madt3125/MainActivity.java
@@ -12,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RegistrationForm.class);
+            startActivity(intent);
+        });
     }
+
 }
