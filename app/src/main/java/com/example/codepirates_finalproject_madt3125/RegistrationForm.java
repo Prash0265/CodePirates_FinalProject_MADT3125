4533d5b1c6be4acca4bf431413792392d261a481
MeetWaraich <rwaraich599@gmail.com>
2022-12-08 21:13:59 -0500

    Updated ID names

--- app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
+++ app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
@@ -3,8 +3,11 @@ package com.example.codepirates_finalproject_madt3125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegistrationForm extends AppCompatActivity {
+
+    EditText firstName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
