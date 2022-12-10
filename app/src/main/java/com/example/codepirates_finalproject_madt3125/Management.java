1f00b5eb0cfd0da5781d155f3663141d549e2d60
Rdhawan25 <dhawan.raghav25@gmail.com>
2022-12-10 18:34:11 -0500

    Added management models

---
+++ app/src/main/java/com/example/codepirates_finalproject_madt3125/Management.java
@@ -0,0 +1,38 @@
+package com.example.codepirates_finalproject_madt3125;
+
+import java.util.ArrayList;
+
+enum EmployeeType {
+    Manager("Manager"),
+    Programmer("Programmer"),
+    Tester("Tester");
+
+
+    private final String label;
+
+    EmployeeType(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
+public class Management {
+    private static final Management databaseInstance = new Management();
+
+    private final ArrayList<Employee> employeesData;
+
+    private Management() {
+        employeesData = new ArrayList<>();
+    }
+
+    public static Management getInstance() {
+        return databaseInstance;
+    }
+
+    public ArrayList<Employee> getCarNamesData() {
+        return employeesData;
+    }
+
+}
