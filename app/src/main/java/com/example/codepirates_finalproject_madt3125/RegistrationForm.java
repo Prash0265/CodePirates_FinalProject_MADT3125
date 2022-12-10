651b91886c533512c7e12f9e79be3137266d9923
Rdhawan25 <dhawan.raghav25@gmail.com>
2022-12-09 19:13:29 -0500

    Added validations for car and motorcycle selection

--- app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
+++ app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
@@ -7,44 +7,65 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class RegistrationForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner employeeTypeSpinner;
-    LinearLayout employeeProperties;
+    LinearLayout employeeProperties, sideCar, carType;
     TextView empSelection;
+    RadioGroup radioGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form);
         employeeTypeSpinner = findViewById(R.id.spnr_employeeType);
         employeeTypeSpinner.setOnItemSelectedListener(this);
+        sideCar = findViewById(R.id.sideCarRdGroup);
+        radioGroup = findViewById(R.id.vehicleTypeRdGroup);
+        carType = findViewById(R.id.carType);
+
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            switch (checkedId) {
+                case R.id.car:
+                    carType.setVisibility(View.VISIBLE);
+                    sideCar.setVisibility(View.GONE);
+                    // group.setOrientation(LinearLayout.HORIZONTAL);
+                    break;
+                case R.id.motorcycle:
+                    carType.setVisibility(View.GONE);
+                    sideCar.setVisibility(View.VISIBLE);
+                    //group.setOrientation(LinearLayout.VERTICAL);
+                    break;
+
+            }
+        });
+
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String item =employeeTypeSpinner.getSelectedItem().toString();
+        String item = employeeTypeSpinner.getSelectedItem().toString();
         if (item.equals("Manager")) {
             employeeProperties = findViewById(R.id.linearEmpProperties);
             employeeProperties.setVisibility(View.VISIBLE);
             empSelection = findViewById(R.id.txtvw_empSelection);
             empSelection.setText("# Clients");
-        }
-        else if (item.equals("Tester")) {
+        } else if (item.equals("Tester")) {
             employeeProperties = findViewById(R.id.linearEmpProperties);
             employeeProperties.setVisibility(View.VISIBLE);
             empSelection = findViewById(R.id.txtvw_empSelection);
             empSelection.setText("# Bugs");
-        }
-        else if (item.equals("Programmer")) {
+        } else if (item.equals("Programmer")) {
             employeeProperties = findViewById(R.id.linearEmpProperties);
             employeeProperties.setVisibility(View.VISIBLE);
             empSelection = findViewById(R.id.txtvw_empSelection);
             empSelection.setText("# Projects");
-        }
-        else {
+        } else {
             employeeProperties = findViewById(R.id.linearEmpProperties);
             employeeProperties.setVisibility(View.GONE);
         }
