53a40ec798863098f4d9c98ed052071fcb3fb918
Rdhawan25 <dhawan.raghav25@gmail.com>
2022-12-10 19:04:29 -0500

    Configured view binding

--- app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
+++ app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
@@ -5,92 +5,79 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class RegistrationForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import com.example.codepirates_finalproject_madt3125.databinding.ActivityRegistrationFormBinding;
 
-    Spinner employeeTypeSpinner;
-    TextView empSelection, employeeProperties,sideCar, carType;
-    RadioGroup radioGroup, sidecarRdGroup;
-    EditText editText_empProperties, editText_carType;
+public class RegistrationForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    ActivityRegistrationFormBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration_form);
-        employeeTypeSpinner = findViewById(R.id.spnr_employeeType);
-        employeeTypeSpinner.setOnItemSelectedListener(this);
-        sideCar = findViewById(R.id.textView5);
-        radioGroup = findViewById(R.id.vehicleType);
-        sidecarRdGroup = findViewById(R.id.sideCarType);
-        carType = findViewById(R.id.textView4);
-        editText_carType = findViewById(R.id.editTextCarType);
+        binding = ActivityRegistrationFormBinding.inflate(getLayoutInflater());
 
-        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+        setContentView(binding.getRoot());
+        binding.spnrEmployeeType.setOnItemSelectedListener(this);
+
+        binding.btnRegister.setOnClickListener(view -> onSubmit());
+
+        binding.vehicleType.setOnCheckedChangeListener((group, checkedId) -> {
             switch (checkedId) {
                 case R.id.car:
-                    //  carType.setVisibility(View.VISIBLE);
-                    carType.setVisibility(View.VISIBLE);
-                    editText_carType.setVisibility(View.VISIBLE);
-                    sidecarRdGroup.setVisibility(View.GONE);
-                    sideCar.setVisibility(View.GONE);
-                    // group.setOrientation(LinearLayout.HORIZONTAL);
+                    binding.editTextCarType.setVisibility(View.VISIBLE);
+                    binding.textView4.setVisibility(View.VISIBLE);
+                    binding.sideCarType.setVisibility(View.GONE);
+                    binding.textView5.setVisibility(View.GONE);
                     break;
                 case R.id.motorcycle:
-                    //  carType.setVisibility(View.GONE);
-                    carType.setVisibility(View.GONE);
-                    editText_carType.setVisibility(View.GONE);
-                    sidecarRdGroup.setVisibility(View.VISIBLE);
-                    sideCar.setVisibility(View.VISIBLE);
-                    //group.setOrientation(LinearLayout.VERTICAL);
+                    binding.editTextCarType.setVisibility(View.GONE);
+                    binding.textView4.setVisibility(View.GONE);
+                    binding.sideCarType.setVisibility(View.VISIBLE);
+                    binding.textView5.setVisibility(View.VISIBLE);
                     break;
 
             }
         });
-
-
     }
+
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String item = employeeTypeSpinner.getSelectedItem().toString();
+        String item = binding.spnrEmployeeType.getSelectedItem().toString();
         switch (item) {
             case "Manager":
-                employeeProperties = findViewById(R.id.txtvw_empSelection);
-                editText_empProperties  = findViewById(R.id.empProperties);
-                employeeProperties.setVisibility(View.VISIBLE);
-                editText_empProperties.setVisibility(View.VISIBLE);
-                empSelection = findViewById(R.id.txtvw_empSelection);
-                empSelection.setText("#Clients");
+                binding.empProperties.setVisibility(View.VISIBLE);
+                binding.txtvwEmpSelection.setVisibility(View.VISIBLE);
+                binding.txtvwEmpSelection.setText("#Clients");
                 break;
             case "Tester":
-                employeeProperties = findViewById(R.id.txtvw_empSelection);
-                editText_empProperties  = findViewById(R.id.empProperties);
-                employeeProperties.setVisibility(View.VISIBLE);
-                editText_empProperties.setVisibility(View.VISIBLE);
-                empSelection.setText("#Bugs");
+                binding.empProperties.setVisibility(View.VISIBLE);
+                binding.txtvwEmpSelection.setVisibility(View.VISIBLE);
+                binding.txtvwEmpSelection.setText("#Bugs");
                 break;
             case "Programmer":
-                employeeProperties = findViewById(R.id.txtvw_empSelection);
-                editText_empProperties  = findViewById(R.id.empProperties);
-                employeeProperties.setVisibility(View.VISIBLE);
-                editText_empProperties.setVisibility(View.VISIBLE);
-                empSelection.setText("#Projects");
+                binding.empProperties.setVisibility(View.VISIBLE);
+                binding.txtvwEmpSelection.setVisibility(View.VISIBLE);
+                binding.txtvwEmpSelection.setText("#Projects");
                 break;
             default:
-                employeeProperties = findViewById(R.id.txtvw_empSelection);
-                editText_empProperties  = findViewById(R.id.empProperties);
-                employeeProperties.setVisibility(View.GONE);
-                editText_empProperties.setVisibility(View.GONE);
+                binding.empProperties.setVisibility(View.GONE);
+                binding.txtvwEmpSelection.setVisibility(View.GONE);
                 break;
         }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    private void onSubmit() {
 
     }
 }
