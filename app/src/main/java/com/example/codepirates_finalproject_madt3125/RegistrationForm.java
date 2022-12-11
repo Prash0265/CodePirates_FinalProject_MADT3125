be7fd60b47762cdb01f31aa5bb9ee9777114d186
MeetWaraich <rwaraich599@gmail.com>
2022-12-10 20:49:22 -0500

    Added registration form validations

--- app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
+++ app/src/main/java/com/example/codepirates_finalproject_madt3125/RegistrationForm.java
@@ -5,14 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.codepirates_finalproject_madt3125.databinding.ActivityRegistrationFormBinding;
+
+import java.text.NumberFormat;
 
 public class RegistrationForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     ActivityRegistrationFormBinding binding;
@@ -53,17 +50,17 @@ public class RegistrationForm extends AppCompatActivity implements AdapterView.O
             case "Manager":
                 binding.empProperties.setVisibility(View.VISIBLE);
                 binding.txtvwEmpSelection.setVisibility(View.VISIBLE);
-                binding.txtvwEmpSelection.setText("#Clients");
+                binding.txtvwEmpSelection.setText("Total Number Of Clients:");
                 break;
             case "Tester":
                 binding.empProperties.setVisibility(View.VISIBLE);
                 binding.txtvwEmpSelection.setVisibility(View.VISIBLE);
-                binding.txtvwEmpSelection.setText("#Bugs");
+                binding.txtvwEmpSelection.setText("Total Number Of Bugs:");
                 break;
             case "Programmer":
                 binding.empProperties.setVisibility(View.VISIBLE);
                 binding.txtvwEmpSelection.setVisibility(View.VISIBLE);
-                binding.txtvwEmpSelection.setText("#Projects");
+                binding.txtvwEmpSelection.setText("Total Number Of Projects:");
                 break;
             default:
                 binding.empProperties.setVisibility(View.GONE);
@@ -77,7 +74,64 @@ public class RegistrationForm extends AppCompatActivity implements AdapterView.O
 
     }
 
-    private void onSubmit() {
+    public boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
+    private void onSubmit() {
+        String empFirstName = binding.firstName.getText().toString();
+        String empLastName = binding.lastName.getText().toString();
+        String empDate = binding.birthYear.getText().toString();
+        String empMonthlySal = binding.monthlySalary.getText().toString();
+        String empOccupationRate = binding.occupationRate.getText().toString();
+        String empId = binding.employeeID.getText().toString();
+        String empType = binding.spnrEmployeeType.getSelectedItem().toString();
+        String empProperties = binding.empProperties.getText().toString();
+
+        String vehicleType = "";
+        if (binding.vehicleType.getCheckedRadioButtonId() == R.id.car) {
+            vehicleType = "Car";
+        } else if (binding.vehicleType.getCheckedRadioButtonId() == R.id.motorcycle) {
+            vehicleType = "Motorcycle";
+        }
+        String carType = binding.editTextCarType.toString();
+        String sideCar = "";
+        if (binding.sideCarType.getCheckedRadioButtonId() == R.id.yes) {
+            vehicleType = "Yes";
+        } else if (binding.sideCarType.getCheckedRadioButtonId() == R.id.no) {
+            vehicleType = "No";
+        }
+        String vehicleModel = binding.vehicleModel.getText().toString();
+        String vehiclePlate = binding.plateNumber.getText().toString();
+        String vehicleColor = binding.vehicleColor.getSelectedItem().toString();
+
+        String msg = "";
+        if (empFirstName.isEmpty()) msg = "Please Enter Your First Name";
+        else if (empLastName.isEmpty()) msg = "Please Enter Your Last Name";
+        else if (empDate.isEmpty()) msg = "Please Enter Your Date Of Birth";
+        else if (!isNumeric(empDate)) msg = "Please Enter Valid Date Of Birth";
+        else if (empMonthlySal.isEmpty()) msg = "Please Enter Your Monthly Salary";
+        else if (!isNumeric(empMonthlySal)) msg = "Please Enter Valid Monthly Salary";
+        else if (empOccupationRate.isEmpty()) msg = "Please Enter Your Occupation Rate";
+        else if (!isNumeric(empOccupationRate)) msg = "Please Enter Valid Occupation Rate";
+        else if (empType.isEmpty()) msg = "Please Select a Employee Type";
+        else if (empProperties.isEmpty()) msg = "Please Enter The Number";
+        else if (!isNumeric(empProperties)) msg = "Please Enter Valid Number";
+        else if (empId.isEmpty()) msg = "Please Enter Your Employee ID";
+        else if (vehicleType.isEmpty()) msg = "Please Select a Vehicle Type";
+        else if (vehicleType.equals("Car") && carType.isEmpty())
+            msg = "Please Enter a Valid Car Type ";
+        else if (vehicleType.equals("Motorcycle") && sideCar.isEmpty())
+            msg = "Please Select a Valid Side Car Type ";
+        else if (vehicleModel.isEmpty()) msg = "Please Enter Vehicle Model ";
+        else if (vehiclePlate.isEmpty()) msg = "Please Enter Vehicle Plate Number";
+        else if (vehicleColor.isEmpty()) msg = "Please Select Vehicle Color";
+
+        if (msg.length() > 0) Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
